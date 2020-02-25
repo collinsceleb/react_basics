c69e70fab0501c240cb3d734827c14b15cbba5d9
Kolawole Meshack Afuye <collinsceleb@gmail.com>
2020-02-25 12:30:39 +0100

    A React App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component.jsx";
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   constructor() {
@@ -10,19 +11,19 @@ class App extends Component {
     };
   }
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json()).then(users => this.setState({ monsters: users }));
-    
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
   render() {
-  return (
-    <div className="App">
-      {
-        this.state.monsters.map(monster => <h1 key= {monster.id}>{monster.name}</h1>)
-      }
-    </div>
-  );
-}
+    return (
+      <div className="App">
+        <CardList monsters={this.state.monsters}>
+          
+        </CardList>
+      </div>
+    );
+  }
 }
 
 export default App;
