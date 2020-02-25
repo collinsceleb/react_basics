31c2540a4c3af6130728712c1711273e1aedd415
Kolawole Meshack Afuye <collinsceleb@gmail.com>
2020-02-25 10:17:25 +0100

    A React App

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein'
-        },
-        {
-          name: 'Dracula'
-        },
-        {
-          name: 'Zombie'
-        },
-      ]
-    }
+      monsters: []
+    };
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json()).then(users => this.setState({ monsters: users }));
+    
   }
   render() {
   return (
     <div className="App">
       {
-        this.state.monsters.map(monster => <h1>{monster.name}</h1>)
+        this.state.monsters.map(monster => <h1 key= {monster.id}>{monster.name}</h1>)
       }
     </div>
   );
