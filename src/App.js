8af1dce2b1c716f7e4885ee9369f90f09686aa0c
Kolawole Meshack Afuye <collinsceleb@gmail.com>
2020-02-27 17:49:21 +0100

    A React App

--- src/App.js
+++ src/App.js
@@ -17,6 +17,9 @@ class App extends Component {
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }));
   }
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter(monster =>
@@ -24,9 +27,10 @@ class App extends Component {
     );
     return (
       <div className="App">
+      <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder="search monsters"
-          handleChange={e => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters}></CardList>
       </div>
